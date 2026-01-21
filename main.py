043c25d8826f97b80cb4517360064cb9cecb7c63
timesnewronann <ronanborja26@gmail.com>
2026-01-20 20:41:29 -0800

    Updated program to store a list of messages in our conversation and pass in a role

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 
@@ -19,12 +20,12 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
 
-    # user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     # agent response buildingbootdev run 3d695968-98c9-4a91-b1e2-0ca53e8826b7
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=args.user_prompt)
+        contents=messages)
     if response.usage_metadata is not None:
         prompt_tokens = response.usage_metadata.prompt_token_count
         candidate_tokens = response.usage_metadata.candidates_token_count
