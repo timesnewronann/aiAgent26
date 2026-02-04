785371c0e55794c1d46755e712d6c2aa0ec9c2d9
timesnewronann <ronanborja26@gmail.com>
2026-02-03 23:22:09 -0800

    Working on more function declarations so LLM can call our other functions

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,6 +1,6 @@
 from google import genai
 from google.genai import types
-from get_files_info import schema_get_files_info
+from functions.get_files_info import schema_get_files_info
 
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info],
