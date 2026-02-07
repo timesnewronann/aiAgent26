eee0bdc1b28b3afb1d6451b33e3a8407f9b9b4f1
timesnewronann <ronanborja26@gmail.com>
2026-02-06 22:47:56 -0800

    Implemented call_function return types.Content

--- functions/call_function.py
+++ functions/call_function.py
@@ -11,10 +11,54 @@ available_functions = types.Tool(
                            schema_run_python_file, schema_write_file],
 )
 
+# function map
+function_map = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+}
+
 
 def call_function(function_call, verbose=False):
-    # If verbose 
-    if verbose == True:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+    # Handle the case if function_name is empty
+    function_name = function_call.name or ""
 
-    print(f" - Calling function: {function_call.name}")
+    # If verbose
+    if verbose == True:
+        print(f"Calling function: {function_name}({function_call.args})")
+    # not verbose
+    else:
+        print(f" - Calling function: {function_name}")
+
+    # check if function name is not in function map -> return types.Content object that explains the error
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    # Create and modify args:
+    args = dict(function_call.args) if function_call.args else {}
+
+    # Set "working_directory" to "./calculator" in the args dictionary
+    args["working_directory"] = "./calculator"
+
+    # Call the function
+    function_result = function_map[function_name](**args)
+
+    # wrap function result in a types.content using types.Part.from_function_response
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
