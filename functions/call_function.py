7429802abecad665e45f83a43499b496a36ed407
timesnewronann <ronanborja26@gmail.com>
2026-02-06 22:23:24 -0800

    Added functions into import

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,9 +1,10 @@
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
+import argparse
 
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info, schema_get_file_content,
@@ -12,4 +13,3 @@ available_functions = types.Tool(
 
 
 def call_function(function_call, verbose=False):
-    
